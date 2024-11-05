3c3c44558bf46288f5bf75e6d678c3a1d9a823d5
Juan Pablo RP <jpabloroldanp@gmail.com>
2024-11-04 20:47:14 -0500

    feat: se crea status entity y se modifica el script de sql

---
+++ src/main/resources/db/migration/V2__datos_defecto.sql
@@ -0,0 +1,25 @@
+
+--INSERT INTO table_name (column1, column2, column3, ...)
+--VALUES (value1, value2, value3, ...);
+
+-- User status
+INSERT INTO status (status_name) VALUES ( 'Pendiente');
+INSERT INTO status (status_name) VALUES ( 'Aceptado');
+INSERT INTO status (status_name) VALUES ( 'Rechazado');
+
+-- User role
+INSERT INTO role (role_name) VALUES ('Super Administrador');
+INSERT INTO role (role_name) VALUES ('Administrador');
+INSERT INTO role (role_name) VALUES ('Gestor de riesgo');
+INSERT INTO role (role_name) VALUES ('Propietario del riesgo');
+INSERT INTO role (role_name) VALUES ('Gestor de activos');
+INSERT INTO role (role_name) VALUES ('Analista de seguridad');
+INSERT INTO role (role_name) VALUES ('Auditor');
+
+
+-- Impact level
+INSERT INTO impact_level (impact_value) VALUES ('Bajo');
+INSERT INTO impact_level (impact_value) VALUES ('Medio');
+INSERT INTO impact_level (impact_value) VALUES ('Alto');
+
+
